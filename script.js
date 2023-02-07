d2e3704975bf476165b3107d9ab3eb4684c74c2a
Sumit Goel <sgoel@caltech.edu>
2023-02-07 10:16:55 +0530

    updated

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+// function toggleAbstract(id) {
+//   var abstract = document.getElementById("abstract-" + id);
+//   var button = document.getElementById("button-" + id);
+//   if (abstract.style.display === "none") {
+//     abstract.style.display = "block";
+//     button.innerHTML = "Hide abstract";
+//   } else {
+//     abstract.style.display = "none";
+//     button.innerHTML = "Show abstract";
+//   }
+// }
+
 function toggleAbstract(id) {
-  var abstract = document.getElementById("abstract-" + id);
-  var button = document.getElementById("button-" + id);
-  if (abstract.style.display === "none") {
-    abstract.style.display = "block";
-    button.innerHTML = "Hide abstract";
-  } else {
+  const abstract = document.getElementById(`abstract-${id}`);
+  const button = document.getElementById(`button-${id}`);
+  if (abstract.style.display === "block") {
     abstract.style.display = "none";
     button.innerHTML = "Show abstract";
+  } else {
+    abstract.style.display = "block";
+    button.innerHTML = "Hide abstract";
   }
 }
+
+
+
