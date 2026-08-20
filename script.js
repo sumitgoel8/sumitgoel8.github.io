dcdd2061ccca9d58085bed4d275c31f0e634c177
Sumit Goel <sumitgoel8@yahoo.co.in>
2026-08-20 23:01:09 +0400

    analytics

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+/* Google Analytics */
+(function () {
+  const measurementId = "G-NX2T0MVJLL";
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag("js", new Date());
+  window.gtag("config", measurementId);
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src =
+    `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+
+  document.head.appendChild(script);
+})();
+
+
 const workingPapers = [
     {
     	id: 11,
